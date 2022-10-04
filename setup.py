0ab2e13ab6887eedba81fa08e51053f19295259b
EnricoGavagnin <gavagnin.enrico@gmail.com>
2022-10-04 10:35:47 +0100

    Added folder structure and setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nesta_ds_utils',
+      version='0.1',
+      description='Utility package for Nesta Data Science Practise',
+      url='https://github.com/nestauk/nesta_ds_utils',
+      author='Nesta DAP',
+      license='MIT',
+      packages=['nesta_ds_utils'])
