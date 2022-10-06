5d3598f51b54c59bdbfb07cf92c55392b3d06f4c
Emily Bicks <emilybicks@NESTA-CQ6PWN4772.home>
2022-10-06 17:04:54 +0100

    making stylistic suggestions and adding options to extractall function

--- nesta_ds_utils/nesta_io.py
+++ nesta_ds_utils/nesta_io.py
@@ -1,13 +1,15 @@
-
 from io import BytesIO
 from typing import Union
-import pathlib
+from pathlib import Path
+from xmlrpc.client import Boolean
 import zipfile
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
-def convert_str_to_pathlib_path(path: Union[pathlib.Path, str]) -> pathlib.Path:
+
+def _convert_str_to_pathlib_path(path: Union[Path, str]) -> Path:
     """Converts a path written as a string to pathlib format
 
     :param path: file path in string format
@@ -15,27 +17,37 @@ def convert_str_to_pathlib_path(path: Union[pathlib.Path, str]) -> pathlib.Path:
     :return: path in pathlib format
     :rtype: pathlib.Path
     """
-    return pathlib.Path(path) if type(path) is str else path
+    return Path(path) if type(path) is str else path
 
-def make_path_if_not_exist(path: Union[pathlib.Path, str]):
+
+def make_path_if_not_exist(path: Union[Path, str]):
     """Check if path exists, if it does not exist then create it
 
     :param path: file path 
     :type path: pathlib.Path or str
     """
-    path = convert_str_to_pathlib_path(path)
+    path = _convert_str_to_pathlib_path(path)
     if not path.exists():
         path.mkdir(parents=True)
 
-def extractall(zipPath: Union[pathlib.Path, str], outPath: Union[pathlib.Path, str]):
-    """Takes path to zipped file and extracts it to specified output path
-    :param zipPath: path to zipped file
-    :type zipPath: str or pathlib.Path
-    :param outPath: path where contents will be unzipped to
-    :type outPath: str or pathlib.Path
-    """
-    logger.info(f"Extracting to {outPath}")
 
-    make_path_if_not_exist(outPath)
-    with zipfile.ZipFile(zipPath,'r') as z:
-        z.extractall(outPath)
+def extractall(zip_path: Union[Path, str], out_path: Union[Path, str]=None, delete_zip: Boolean = True):
+    """Takes path to zipped file and extracts it to specified output path
+    :param zip_path: path to zipped file
+    :type zip_path: str or pathlib.Path
+    :param out_path: path where contents will be unzipped to
+    :type out_path: str or pathlib.Path
+    :param delete_zip: option to delete zip file after extracted
+    :type delete_zip: Boolean
+    """
+    logger.info(f"Extracting to {out_path}")
+
+    if out_path is None:
+        out_path = zip_path.rsplit("/", 1)[0]
+
+    make_path_if_not_exist(out_path)
+    with zipfile.ZipFile(zip_path, "r") as z:
+        z.extractall(out_path)
+    
+    if delete_zip is True:
+        os.remove(zip_path)
