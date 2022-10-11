5e4593d55155af9466ddc444e03ad4331b8d3cc0
Emily Bicks <emilybicks@NESTA-CQ6PWN4772.home>
2022-10-11 15:02:11 +0100

    pulling from 31

--- setup.py
+++ setup.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python
  
+from platform import python_version
 from setuptools import setup
 
 if __name__ == "__main__":
-      setup({extras_require={
-      'dev': [
-      "Sphinx==5.2.3",
-      "sphinxcontrib-applehelp==1.0.2",
-      "sphinxcontrib-devhelp==1.0.2",
-      "sphinxcontrib-htmlhelp==2.0.0",
-      "sphinxcontrib-jsmath==1.0.1",
-      "sphinxcontrib-qthelp==1.0.3",
-      "sphinxcontrib-serializinghtml==1.1.5",
-      "pytest==7.1.3"
-        ]
-    })
+  setup()
