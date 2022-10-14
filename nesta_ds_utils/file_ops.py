1163d6934aade9be023778a2cb4ed5699b9b7802
EnricoGavagnin <gavagnin.enrico@gmail.com>
2022-10-14 12:20:16 +0100

    Added new Download from & Upload to S3

--- nesta_ds_utils/file_ops.py
+++ nesta_ds_utils/file_ops.py
@@ -1,18 +1,12 @@
-from io import BytesIO
-from typing import Union
+import io 
+from typing import Union, List
 from pathlib import Path
 from xmlrpc.client import Boolean
 import zipfile
 import os
 import boto3
 from fnmatch import fnmatch
-import pickle
-import json
-from typing import List
 import pandas as pd
-import io
-import csv
-S3 = boto3.resource("s3")
 
 
 def _convert_str_to_pathlib_path(path: Union[Path, str]) -> Path:
@@ -28,7 +22,6 @@ def _convert_str_to_pathlib_path(path: Union[Path, str]) -> Path:
 
 def make_path_if_not_exist(path: Union[Path, str]):
     """Check if path exists, if it does not exist then create it.
-
     :param path: file path 
     :type path: pathlib.Path or str
     """
@@ -62,7 +55,7 @@ def extractall(
         os.remove(zip_path)
 
 
-def get_s3_dir_files(bucket_name: str, dir_name: str='') -> List[str]:
+def get_dir_files_s3(bucket_name: str, dir_name: str='') -> List[str]:
     """Get a list of all files in bucket directory.
 
     Args:
@@ -72,86 +65,84 @@ def get_s3_dir_files(bucket_name: str, dir_name: str='') -> List[str]:
     Returns:
         List[str]: List of file names in bucket directory
     """
-
-    my_bucket = S3.Bucket(bucket_name)
+    s3_resources = boto3.resource("s3")
+    my_bucket = s3_resources.Bucket(bucket_name)
     return [
         object_summary.key
         for object_summary in my_bucket.objects.filter(Prefix=dir_name)
     ]
 
 
-def load_from_s3(bucket_name: str, file_name: str) -> Union[pd.DataFrame, str, dict]:
-    """Load data from S3 location.
-
-    Args:
-        bucket_name (str): The S3 bucket name
-        file_name (str): S3 key to load
-
-    Returns:
-        Union[pd.DataFrame, str, dict]: Loaded data from S3 location.
-    """
-
-    obj = S3.Object(bucket_name, file_name)
-    if fnmatch(file_name, "*.csv") or fnmatch(file_name, "*.tsv"):
-        return pd.read_csv(f"s3://{bucket_name}/{file_name}")
-    elif fnmatch(file_name, "*.tsv.zip"):
-        return pd.read_csv(
-            f"s3://{bucket_name}/{file_name}",
-            compression="zip",
-            sep="\t",
-        )
-    elif fnmatch(file_name, "*.pickle") or fnmatch(file_name, "*.pkl"):
-        file = obj.get()["Body"].read()
-        return pickle.loads(file)
-    elif fnmatch(file_name, "*.txt"):
-        file = obj.get()["Body"].read().decode()
-        return [f.split("\t") for f in file.split("\n")]
-    elif fnmatch(file_name, "*.json"):
-        file = obj.get()["Body"].read().decode()
-        return json.loads(file)
-    else:
-        raise Exception( 
-        'Function not supported for file type other than "*.json", *.txt", "*.pickle", "*.tsv" and "*.csv"'
-        )
-
-
-def save_to_s3(bucket_name: str, output_file_dir: str, output_var: Union[pd.DataFrame, str, dict]):
-    """Save data to S3 location.
+def df_to_fileobj(df: pd.DataFrame, save_file_dir: str) -> io.BytesIO:
+    """Convert DataFrame into bytes file object
     
     Args:
-        bucket_name (str): The S3 bucket name
-        output_file_dir (str): File path to save object to
-        output_var (Union[pd.DataFrame, str, dict]): Data to be saved
+        df (pd.DataFrame): Dataframe to convert 
+        save_file_dir (io.BytesIO): Saving file name specifying format
+
+    Returns:
+        io.BytesIO: Bytes file object
     """
+    buffer = io.BytesIO()
+    if fnmatch(save_file_dir, "*.csv"):
+        df.to_csv(buffer)
+    elif fnmatch(save_file_dir, "*.json"):
+        df.to_json(buffer) 
+    buffer.seek(0)
+    return buffer
 
-    obj = S3.Object(bucket_name, output_file_dir)
 
-    if fnmatch(output_file_dir, "*.pkl") or fnmatch(output_file_dir, "*.pickle"):
-        obj.put(Body=pickle.dumps(output_var))
-    elif fnmatch(output_file_dir, "*.txt"):
-        if not isinstance(output_var, str):
-            raise Exception(
-                'Saving in .txt format is supported only for \'str\' data.'
-            )
-        obj.put(Body=output_var)
-    elif fnmatch(output_file_dir, "*.csv"):
-        if isinstance(output_var, pd.DataFrame):
-            output_var.to_csv("s3://" + bucket_name + "/" + output_file_dir, index=False)
-        elif isinstance(output_var, dict):
-            csv_stream = io.StringIO()
-            with csv_stream as f:  
-                w = csv.DictWriter(f, output_var.keys())
-                w.writeheader()
-                w.writerow(output_var)
-                obj.put(Body=csv_stream.getvalue())
-        else:
-            obj.put(Body=output_var)          
-    elif fnmatch(output_file_dir, "*.json"):
-        if isinstance(output_var, pd.DataFrame):
-            output_var.to_json("s3://" + bucket_name + "/" + output_file_dir)
-        else:
-            obj.put(Body=json.dumps(output_var))
+def fileobj_to_df(fileobj: io.BytesIO, load_file_dir: str) -> pd.DataFrame:
+    """Convert bytes file object into DataFrame
+
+    Args:
+        buffer (io.BytesIO): Bytes file object
+        file_dir (str): Loading file name specifying format
+
+    Returns:
+        pd.DataFrame: Dataframe converted
+    """
+    if fnmatch(load_file_dir, "*.csv"):
+        df = pd.read_csv(fileobj)
+    elif fnmatch(load_file_dir, "*.json"):
+        df = pd.read_json(fileobj) 
+    return df
+
+
+def upload_data_s3(data: Union[io.BytesIO, pd.DataFrame], bucket: str, save_file_path: str):
+    """Upload data to S3 location
+
+    Args:
+        data (Union[io.BytesIO, pd.DataFrame]): Data to upload
+        bucket (str): Bucket's name
+        save_file_path (str): File path to saveing data
+    """
+    s3 = boto3.client('s3')
+    if isinstance(data, pd.DataFrame):
+        obj =  df_to_fileobj(data, save_file_path)
+        s3.upload_fileobj(obj, bucket, save_file_path)      
+    elif isinstance(data, io.BytesIO):
+        s3.upload_fileobj(data, bucket, save_file_path)
     else:
         raise Exception(
-            'Function not supported for file type other than "*.json", *.txt", "*.pickle" and "*.csv"'
+            'Function supports data only as "io.BytesIO" or "io.DataFrame"'
         )
+
+
+def download_data_s3(bucket: str, file_path: str, asDataFrame: bool=False) -> Union[io.BytesIO, pd.DataFrame]:
+    """Download data from S3 location
+
+    Args:
+        bucket (str): Bucket's name
+        file_path (str): File path to loading data
+        asDataFrame (bool, optional): If True: return the data as pd.DataFrame. If False: return data as io.BytesIO. Default: False. 
+
+    Returns:
+        Union[io.BytesIO, pd.DataFrame]: Downloaded data
+    """
+    s3 = boto3.client('s3')
+    fileobj= io.BytesIO()
+    s3.download_fileobj(bucket, file_path, fileobj)
+    fileobj.seek(0)
+    return (fileobj_to_df(fileobj, file_path) 
+            if asDataFrame else fileobj)
