af606363e338b5b030cfdce9305219db6d99b79d
EnricoGavagnin <gavagnin.enrico@gmail.com>
2022-10-27 16:48:52 +0100

    Change `plotting.save` to ccept bool variable, adjusted tests and other minor tweaks - precommits fixed

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -6,33 +6,36 @@ import altair as alt
 import pytest
 
 
-def test_save_altair():
-    """Test that that figures saved exist."""
+def test_save__png_altair():
+    """Test that that figures saved as png exist."""
     fig = alt.Chart(pd.DataFrame()).mark_bar()
     path = "tests/temp/"
     plotting.save(fig, "test_fig", path)
-    assert all(
-        [
-            Path(f"tests/temp/png/test_fig.png").exists(),
-            Path(f"tests/temp/svg/test_fig.svg").exists(),
-            Path(f"tests/temp/html/test_fig.html").exists(),
-        ]
-    )
+    assert Path(f"tests/temp/test_fig.png").exists()
     shutil.rmtree("tests/temp/")
 
 
-def test_save_altair():
-    """Test that that figures saved exist."""
+def test_save_html_altair():
+    """Test that that figures saved as html exist."""
     fig = alt.Chart(pd.DataFrame()).mark_bar()
     path = "tests/temp/"
-    plotting.save(fig, "test_fig", path, filetypes="html")
-    assert Path(f"tests/temp/html/test_fig.html").exists()
+    plotting.save(fig, "test_fig", path, save_png=False, save_html=True)
+    assert Path(f"tests/temp/test_fig.html").exists()
+    shutil.rmtree("tests/temp/")
+
+
+def test_save_svg_altair():
+    """Test that that figures saved as svg exist."""
+    fig = alt.Chart(pd.DataFrame()).mark_bar()
+    path = "tests/temp/"
+    plotting.save(fig, "test_fig", path, save_png=False, save_svg=True)
+    assert Path(f"tests/temp/test_fig.svg").exists()
     shutil.rmtree("tests/temp/")
 
 
 def test_save_altair_exception():
-    """Test that function returns exception for unsupported formats."""
+    """Test that function returns exception no format is selected."""
     fig = alt.Chart(pd.DataFrame()).mark_bar()
     path = "tests/temp/"
     with pytest.raises(Exception):
-        plotting.save(fig, "test_fig", path, filetypes="")
+        plotting.save(fig, "test_fig", path, save_png=False)
