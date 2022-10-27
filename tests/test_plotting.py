ff65bdecf2a58f87bf61d046e72320295d154c16
EnricoGavagnin <gavagnin.enrico@gmail.com>
2022-10-27 11:28:23 +0100

    Allow str type as filetypes in plotting.save- precommit fixed

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -21,9 +21,18 @@ def test_save_altair():
     shutil.rmtree("tests/temp/")
 
 
+def test_save_altair():
+    """Test that that figures saved exist."""
+    fig = alt.Chart(pd.DataFrame()).mark_bar()
+    path = "tests/temp/"
+    plotting.save(fig, "test_fig", path, filetypes="html")
+    assert Path(f"tests/temp/html/test_fig.html").exists()
+    shutil.rmtree("tests/temp/")
+
+
 def test_save_altair_exception():
     """Test that function returns exception for unsupported formats."""
     fig = alt.Chart(pd.DataFrame()).mark_bar()
     path = "tests/temp/"
     with pytest.raises(Exception):
-        plotting.save(fig, "test_fig", path, filetypes=[""])
+        plotting.save(fig, "test_fig", path, filetypes="")
