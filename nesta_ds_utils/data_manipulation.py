58ddb423fca931f41a9bb7ad50d3be1489d74a7c
Emily Bicks <emilybicks@NESTA-CQ6PWN4772.local>
2022-11-02 13:27:59 +0000

    allowing users to specify null values or list of possible date options

--- nesta_ds_utils/data_manipulation.py
+++ nesta_ds_utils/data_manipulation.py
@@ -8,36 +8,56 @@ from typing import Union
 def parse_date_string(
     date_string: str,
     format: Union[str, list] = "%Y-%m-%d",
-    null_value: Union[float, str] = np.nan,
+    error_value: Union[float, str] = np.nan,
 ) -> datetime.datetime:
     """parses a date string into a datetime object
 
     Args:
         date_string (str): string describing a date.
-        format (str, optional): date format. Defaults to "%Y-%m-%d".
+        format (str or list, optional): date format or list of possible date formats. Defaults to "%Y-%m-%d".
         For additional date format options refer to `datetime documentation <https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes>`_.
-
+        error_value (float or str, optional): value to use for null or improperly formatted values. Defaults to numpy.nan.
 
     Returns:
         datetime.datetime: date in datetime format if date_string matches date format,
         otherwise returns None.
     """
-    try:
-        return datetime.datetime.strptime(date_string, format)
+    if isinstance(format, str):
+        try:
+            return datetime.datetime.strptime(date_string, format)
 
-    except ValueError:
-        sys.stderr.write(
-            "Date string {} did not match date format {}".format(date_string, format)
-        )
-        return np.nan
-
-    except TypeError:
-        sys.stderr.write(
-            "argument {} passed as date_string was type {} not string".format(
-                date_string, type(date_string)
+        except ValueError:
+            sys.stderr.write(
+                "Date string {} did not match date format {}".format(
+                    date_string, format
+                )
             )
-        )
-        return np.nan
+            return error_value
+
+        except TypeError:
+            sys.stderr.write(
+                "argument {} passed as date_string was type {} not string".format(
+                    date_string, type(date_string)
+                )
+            )
+            return error_value
+
+    else:
+        for format_option in format:
+            try:
+                return datetime.datetime.strptime(date_string, format)
+
+            except TypeError:
+                sys.stderr.write(
+                    "argument {} passed as date_string was type {} not string".format(
+                        date_string, type(date_string)
+                    )
+                )
+                return error_value
+
+            except ValueError:
+                continue
+        return error_value
 
 
 def make_year(date: datetime.datetime) -> int:
