a82bfee2226cd10036ff86cdc907bb20285ef581
EnricoGavagnin <gavagnin.enrico@gmail.com>
2022-11-15 08:59:15 +0000

    New download_obj util

--- nesta_ds_utils/file_ops.py
+++ nesta_ds_utils/file_ops.py
@@ -13,6 +13,7 @@ import pyarrow.parquet as pq
 import json
 import pickle
 from dicttoxml import dicttoxml
+import xmltodict
 import warnings
 
 
@@ -117,7 +118,7 @@ def _dict_to_fileobj(dict_data: dict, path_to: str, **kwargs) -> io.BytesIO:
     if fnmatch(path_to, "*.json"):
         buffer.write(json.dumps(dict_data, **kwargs).encode())
     elif fnmatch(path_to, ".xml"):
-        buffer.write(dicttoxml(dict_data, **kwargs))
+        buffer.write(dicttoxml(dict_data, attr_type=False, **kwargs))
     else:
         raise Exception(
             "Uploading dictionary currently supported only for 'json' and 'xml'."
@@ -143,7 +144,7 @@ def _list_to_fileobj(list_data: list, path_to: str, **kwargs) -> io.BytesIO:
         for row in list_data:
             buffer.write(bytes(str(row) + "\n", "utf-8"))
     elif fnmatch(path_to, ".json"):
-        buffer.write(json.dumps(dict_data, **kwargs).encode())
+        buffer.write(json.dumps(list_data, **kwargs).encode())
     else:
         raise Exception(
             "Uploading list currently supported only for 'csv', 'txt' and 'json'."
@@ -246,7 +247,7 @@ def upload_obj(
     else:
         obj = _unsupp_data_to_fileobj(obj, path_to, **kwargs_writing)
         warnings.warn(
-            "Data uploaded using pickle. Please consider other accessible "
+            "Data uploaded as pickle. Please consider other accessible "
             "file types among the suppoted ones."
         )
 
@@ -254,71 +255,192 @@ def upload_obj(
     s3.upload_fileobj(obj, bucket, path_to, **kwargs_upload)
 
 
-def _fileobj_to_df(fileobj: io.BytesIO, load_file_dir: str, **kwargs) -> pd.DataFrame:
-    """Convert bytes file object into DataFrame.
+def _fileobj_to_df(fileobj: io.BytesIO, path_from: str, **kwargs) -> pd.DataFrame:
+    """Convert bytes file object into dataframe.
 
     Args:
-        buffer (io.BytesIO): Bytes file object.
-        load_file_dir (str): Loading file name.
+        fileobj (io.BytesIO): Bytes file object.
+        path_from (str): Path of loaded data.
 
     Returns:
-        pd.DataFrame: Dataframe converted
+        pd.DataFrame: Data as dataframe.
     """
-    if fnmatch(load_file_dir, "*.csv"):
-        df = pd.read_csv(fileobj, **kwargs)
-    elif fnmatch(load_file_dir, "*.parquet"):
-        df = pd.read_parquet(fileobj, **kwargs)
-    else:
-        raise Exception(
-            "Automatic convertion into pd.DataFrame currently supported "
-            "only for 'csv'and 'parquet'."
-        )
-    return df
+    if fnmatch(path_from, "*.csv"):
+        return pd.read_csv(fileobj, **kwargs)
+    elif fnmatch(path_from, "*.parquet"):
+        return pd.read_parquet(fileobj, **kwargs)
+
+
+def _fileobj_to_dict(fileobj: io.BytesIO, path_from: str, **kwargs) -> dict:
+    """Convert bytes file object into dictionary.
+
+    Args:
+        fileobj (io.BytesIO): Bytes file object.
+        path_from (str): Path of loaded data.
+
+    Returns:
+        dict: Data as dictionary.
+    """
+    if fnmatch(path_from, "*.json"):
+        dict_data = json.loads(fileobj.getvalue().decode(), **kwargs)
+    elif fnmatch(path_from, "*.xml"):
+        dict_data = xmltodict.parse(fileobj.getvalue(), **kwargs)["root"]
+
+    return dict_data
+
+
+def _fileobj_to_list(fileobj: io.BytesIO, path_from: str, **kwargs) -> list:
+    """Convert bytes file object into list.
+
+    Args:
+        fileobj (io.BytesIO): Bytes file object.
+        path_from (str): Path of loaded data.
+
+    Returns:
+        list: Data as list.
+    """
+    if fnmatch(path_from, "*.csv"):
+        list_data = pd.read_csv(fileobj, **kwargs)["0"].to_list()
+    elif fnmatch(path_from, "*.txt"):
+        list_data = fileobj.read().decode().splitlines()
+    elif fnmatch(path_from, "*.json"):
+        list_data = json.loads(fileobj.getvalue().decode())
+
+    return list_data
+
+
+def _fileobj_to_str(fileobj: io.BytesIO) -> str:
+    """Convert bytes file object into string.
+
+    Args:
+        fileobj (io.BytesIO): Bytes file object.
+        path_from (str): Path of loaded data.
+
+    Returns:
+        str: Data as string.
+    """
+    return fileobj.getvalue().decode("utf-8")
+
+
+def _fileobj_to_np_array(fileobj: io.BytesIO, path_from: str, **kwargs) -> np.ndarray:
+    """Convert bytes file object into numpy array.
+
+    Args:
+        fileobj (io.BytesIO): Bytes file object.
+        path_from (str): Path of loaded data.
+
+    Returns:
+        np.ndarray: Data as numpy array.
+    """
+    if fnmatch(path_from, "*.csv"):
+        np_array_data = np.genfromtxt(fileobj, delimiter=",", **kwargs)
+    elif fnmatch(path_from, "*.parquet"):
+        np_array_data = pq.read_table(fileobj, **kwargs)["data"].to_numpy()
+
+    return np_array_data
 
     obj = S3.Object(bucket_name, output_file_dir)
 
 def download_obj(
-    bucket: str, file_name_from: str, asDataFrame: bool = False, **kwargs
-) -> Union[io.BytesIO, pd.DataFrame]:
+    bucket: str,
+    path_from: str,
+    download_as: str = None,
+    kwargs_download: dict = None,
+    kwargs_reading: dict = None,
+) -> any:
     """Download data to memory from S3 location.
 
     Args:
         bucket (str): Bucket's name.
-        file_name_from (str): Path to data in S3.
-        asDataFrame (bool, optional): If True: return the data as pd.DataFrame.
-        If False: return data as io.BytesIO. Default: False.
+        path_from (str): Path to data in S3.
+        download_as (str, optional): Type of object downloading. Choose between
+        ('dataframe', 'dict', 'list', 'str', 'np.array'). Not needed for 'pkl files'.
+        kwargs_download (dict, optional): Dictionary of kwargs for boto3 function 'download_fileobj'.
+        kwargs_reading (dict, optional): Dictionary of kwargs for reading data.
 
     Returns:
-        Union[io.BytesIO, pd.DataFrame]: Donwloaded data.
+        any: Donwloaded data.
     """
+    if not path_from.endswith(
+        tuple([".csv", ".parquet", ".json", ".xml", ".txt", ".pkl"])
+    ):
+        raise Exception(
+            "This file type is not currently supported for download in memory."
+        )
     s3 = boto3.client("s3")
     fileobj = io.BytesIO()
-    s3.download_fileobj(bucket, file_name_from, fileobj)
+    s3.download_fileobj(bucket, path_from, fileobj, **kwargs_download)
     fileobj.seek(0)
-    return _fileobj_to_df(fileobj, file_name_from, **kwargs) if asDataFrame else fileobj
+    if download_as == "dataframe":
+        if path_from.endswith(tuple([".csv", ".parquet"])):
+            return _fileobj_to_df(fileobj, path_from, **kwargs_reading)
+        else:
+            raise Exception(
+                "Download as dataframe currently supported only "
+                "for 'csv' and 'parquet'."
+            )
+    elif download_as == "dict":
+        if path_from.endswith(tuple([".json", ".xml"])):
+            return _fileobj_to_dict(fileobj, path_from, **kwargs_reading)
+        else:
+            raise Exception(
+                "Download as dictionary currently supported only "
+                "for 'json' and 'xml'."
+            )
+    elif download_as == "list":
+        if path_from.endswith(tuple([".csv", ".txt", ".json"])):
+            return _fileobj_to_list(fileobj, path_from, **kwargs_reading)
+        else:
+            raise Exception(
+                "Download as list currently supported only "
+                "for 'csv', 'txt' and 'json'."
+            )
+    elif download_as == "str":
+        if path_from.endswith(tuple([".txt"])):
+            return _fileobj_to_str(fileobj)
+        else:
+            raise Exception("Download as string currently supported only " "for 'txt'.")
+    elif download_as == "np.array":
+        if path_from.endswith(tuple([".csv", ".parquet"])):
+            return _fileobj_to_np_array(fileobj, path_from, **kwargs_reading)
+        else:
+            raise Exception(
+                "Download as numpy array currently supported only "
+                "for 'csv' and 'parquet'."
+            )
+    elif not download_as:
+        if path_from.endswith(tuple([".pkl"])):
+            return pickle.load(fileobj, **kwargs_reading)
+        else:
+            raise Exception("'download_as' is required for this file type.")
+    else:
+        raise Exception(
+            "'download_as' not supported. Choose between ('dataframe', "
+            "'dict', 'list', 'str', 'np.array'). Not needed for 'pkl files'.'"
+        )
 
 
-def upload_file(file_name_from: str, bucket: str, file_name_to: str, **kwargs):
+def upload_file(path_from: str, bucket: str, path_to: str, **kwargs):
     """Upload local file from disk to S3 location.
 
     Args:
-        file_name_from (str): Path to local file.
+        path_from (str): Path to local file.
         bucket (str): Bucket's name.
-        file_name_to (str): Destination path in S3.
+        path_to (str): Destination path in S3.
     """
     s3 = boto3.client("s3")
-    s3.upload_file(file_name_from, bucket, file_name_to, kwargs)
+    s3.upload_file(path_from, bucket, path_to, kwargs)
 
 
-def download_file(file_name_from: str, bucket: str, file_name_to: str, **kwargs):
+def download_file(path_from: str, bucket: str, path_to: str, **kwargs):
     """Download data from S3 to local file on disk.
 
     Args:
-        file_name_from (str): Path to data in S3
+        path_from (str): Path to data in S3
         bucket (str): Buket's name
-        file_name_to (str): Destination path to disk.
+        path_to (str): Destination path to disk.
     """
     # Create folder if not existing
-    make_path_if_not_exist(_convert_str_to_pathlib_path(file_name_to).parents[0])
+    make_path_if_not_exist(_convert_str_to_pathlib_path(path_to).parents[0])
     s3 = boto3.client("s3")
-    s3.download_file(bucket, file_name_from, file_name_to, kwargs)
+    s3.download_file(bucket, path_from, path_to, kwargs)
