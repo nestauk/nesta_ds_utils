ad9c12248a7610b1f9e5af99c6e756144129a881
Emily Bicks <emilybicks@NESTA-CQ6PWN4772.home>
2022-11-29 16:19:02 +0000

    fixing docs and tests

--- docs/source/generate.rst
+++ docs/source/generate.rst
@@ -3,4 +3,4 @@ generate
 ========
 **generate** contains functions to create networks
 
-.. autofunction:: nesta_ds_utils.networks.generate.term_cooccurrence_graph
+.. autofunction:: nesta_ds_utils.networks.generate.build_coocc
--- tests/networks/test_generate.py
+++ tests/networks/test_generate.py
@@ -1,13 +1,6 @@
 import pytest
-from nesta_ds_utils.networks.generate import term_cooccurrence_graph
+from nesta_ds_utils.networks.generate import build_coocc
 import numpy as np
-
-
-def test_network_from_string_list():
-    """tests that when a list of strings is passed to term_cooccurence_graph it returns the expected edge weights"""
-    sequence = ["I went to the party", "i had fun at the party"]
-    network = term_cooccurrence_graph(sequence)
-    assert network["the"]["party"]["weight"] == 2
 
 
 def test_network_from_nested_lists():
@@ -16,7 +9,7 @@ def test_network_from_nested_lists():
         ["I", "went", "to", "the", "party"],
         ["i", "had", "fun", "at", "the", "party"],
     ]
-    network = term_cooccurrence_graph(sequence)
+    network = build_coocc(sequence)
     assert network["the"]["party"]["weight"] == 2
 
 
@@ -26,30 +19,5 @@ def test_network_from_nested_arrays():
         np.array(["I", "went", "to", "the", "party"]),
         np.array(["i", "had", "fun", "at", "the", "party"]),
     ]
-    network = term_cooccurrence_graph(sequence)
+    network = build_coocc(sequence)
     assert network["the"]["party"]["weight"] == 2
-
-
-def test_network_with_cases():
-    """tests that when lowercase=False the tokens are treated differently based on casing"""
-    sequence = [
-        ["I", "went", "to", "the", "party"],
-        ["i", "had", "fun", "at", "the", "party"],
-    ]
-    network = term_cooccurrence_graph(sequence, lowercase=False)
-    node_list = list(network.nodes)
-    assert ("I" in node_list) and ("i" in node_list)
-
-
-def test_network_with_stopwords():
-    """tests that when a list of stopwords is passed to the network, the tokens are not in the node list"""
-    stop_words = ["I", "had", "at"]
-    sequence = [
-        ["I", "went", "to", "the", "party"],
-        ["i", "had", "fun", "at", "the", "party"],
-    ]
-    network = term_cooccurrence_graph(sequence, stopwords=stop_words)
-    node_list = list(network.nodes)
-    assert (
-        ("i" not in node_list) and ("had" not in node_list) and ("at" not in node_list)
-    )
