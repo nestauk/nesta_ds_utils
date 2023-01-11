a0ef861dd1e1ac03d65a90918392ad1efb66922d
emily-bicks <113300626+emily-bicks@users.noreply.github.com>
2023-01-11 11:49:14 +0000

    adding similarity metrics to edge weights (#79)
    * adding jaccard similarity option to edge weights
    
    * fixing tests
    
    * checking for edge in either order in jaccard sim test
    
    * tweaking jaccard formula
    
    * adding association strength
    
    * adding cosine
    
    * adding inclusion index
    
    * add minimum node frequency parameter
    
    * removing min_node_frequency option
    
    * shortening input parameter names
    
    * adding edges by unpacking a single dictionary and using add_edges_from rather than two sets of conditionals
    
    * specifying _node_frequency_weights
    
    * changing language from terms to tokens
    
    * change tests from using words to using integers
    
    * Adding tests that attributes exist
    
    Co-authored-by: Emily Bicks <emilybicks@NESTA-CQ6PWN4772.home>
    Co-authored-by: Emily Bicks <emilybicks@NESTA-CQ6PWN4772.local>

--- nesta_ds_utils/networks/build.py
+++ nesta_ds_utils/networks/build.py
@@ -6,31 +6,33 @@ from collections import Counter, defaultdict
 from itertools import chain, combinations
 import networkx as nx
 import scipy
+import math
 
 
 def build_coocc(
     sequences: Union[List[list], List[np.array]],
     graph_type: str = "networkx",
-    weighted: bool = True,
     directed: bool = False,
     as_adj: bool = False,
     use_node_weights: bool = False,
+    edge_attributes: List = [],
 ) -> Union[nx.Graph, scipy.sparse._csr.csr_matrix]:
-    """generates a co-occurence graph based on pairwise co-occurence of terms.
+    """generates a co-occurence graph based on pairwise co-occurence of tokens.
 
     Args:
         sequences (Union[List[list], List[np.array]]):
             list of lists or list of numpy arrays containing tokens to use as nodes in the network.
         graph_type (str, optional): Python library to use for network generation.
             Currently only supports 'networkx' but future development should support 'graph-tool'
-        weighted (bool, optional): parameter to indicate of edges should be weighted. Defaults to True.
-            If True, edge weights represent number of co-occurences.
         directed (bool, optional): parameter to indicate if edges should be directed. Defaults to False.
             If True, creates a symmetric graph.
         as_adj (bool, optional): parameter to indicate if network should be returned as an adjacency matrix
             rather than a Graph object.
         use_node_weights (bool, optional): parameter to indicate if node frequency should be added as
             a node attribute.
+        edge_attributes (List, optional): parameter to specify any attributes to add to the edges of the network.
+            Available options are 'frequency', 'jaccard', 'association', 'cosine', or 'inclusion'.
+            Defaults to []. Functions are based on van Eck and Waltman, 2009.
 
     Returns:
         nx.Graph: Returns networkx graph object. If directed=True returns nx.DiGraph, otherwise returns nx.Graph.
@@ -47,17 +49,38 @@ def build_coocc(
     network.add_nodes_from(nodes)
 
     # if using node weights, weights will represent frequency in the corpus
+
     if use_node_weights:
         node_weights = Counter(all_tokens)
         nx.set_node_attributes(network, node_weights, "frequency")
 
     # edge weights are all times a pair of tokens have co-occured in the same sequence
-    edge_weights = _cooccurrence_counts(sequences)
-    for key, val in edge_weights.items():
-        weight = {"weight": val} if weighted else {}
-        network.add_edge(key[0], key[1], **weight)
-        if directed:
-            network.add_edge(key[1], key[0], **weight)
+    cooccurrences = _cooccurrence_counts(sequences)
+
+    edges = {x: defaultdict() for x in cooccurrences.keys()}
+
+    # if using similarity metrics as edge attributes, calculate those
+    if "frequency" in edge_attributes:
+        for node, freq in cooccurrences.items():
+            edges[node]["frequency"] = freq
+    if "jaccard" in edge_attributes:
+        for node, sim in _jaccard_similarity(cooccurrences, all_tokens).items():
+            edges[node]["jaccard_similarity"] = sim
+    if "association" in edge_attributes:
+        for node, sim in _association_strength(cooccurrences, all_tokens).items():
+            edges[node]["association_strength"] = sim
+    if "cosine" in edge_attributes:
+        for node, sim in _cosine_sim(cooccurrences, all_tokens).items():
+            edges[node]["cosine_similarity"] = sim
+    if "inclusion" in edge_attributes:
+        for node, sim in _inclusion_index(cooccurrences, all_tokens).items():
+            edges[node]["inclusion_index"] = sim
+
+    # add edges to network
+    network.add_edges_from(list((u, v, edges[(u, v)]) for u, v in edges.keys()))
+
+    if directed:
+        network.add_edges_from(list((v, u, edges[(u, v)]) for u, v in edges.keys()))
 
     # if as_adj is true this will return a sparse matrix, otherwise it will return a networkx graph
     if as_adj:
@@ -78,3 +101,91 @@ def _cooccurrence_counts(sequences):
     """
     combos = (combinations(sorted(set(sequence)), 2) for sequence in sequences)
     return Counter(chain(*combos))
+
+
+def _node_frequency_counts(all_tokens: List) -> dict:
+    """counts frequency of all tokens in corpus
+
+    Args:
+        all_tokens (List): list of all tokens in corpus
+
+    Returns:
+        dict: key: token, value: frequency
+    """
+    return Counter(all_tokens)
+
+
+def _jaccard_similarity(edge_weights: dict, all_tokens: list) -> dict:
+    """calculate the jaccard similarity between nodes in the network
+
+    Args:
+        edge_weights (dict): co-occurence counts of the nodes in the network
+        all_tokens (list): list of all tokens in the corpus
+
+    Returns:
+        dict: key: pair of tokens, value: jaccard similarity
+    """
+    token_frequency = _node_frequency_counts(all_tokens)
+    jaccard_sims = defaultdict(int)
+    for key, val in edge_weights.items():
+        jaccard_sims[key] = val / (
+            (token_frequency[key[0]] + token_frequency[key[1]]) - val
+        )
+    return jaccard_sims
+
+
+def _association_strength(edge_weights: dict, all_tokens: list) -> dict:
+    """calculate the association strength between nodes in the network
+
+    Args:
+        edge_weights (dict): co-occurence counts of the nodes in the network
+        all_tokens (list): list of all tokens in the corpus
+
+    Returns:
+        dict: key: pair of tokens, value: association strength
+    """
+    token_frequency = _node_frequency_counts(all_tokens)
+    association_strengths = defaultdict(int)
+    for key, val in edge_weights.items():
+        association_strengths[key] = val / (
+            token_frequency[key[0]] * token_frequency[key[1]]
+        )
+    return association_strengths
+
+
+def _cosine_sim(edge_weights: dict, all_tokens: list) -> dict:
+    """calculate the cosine similarity between nodes in the network
+
+    Args:
+        edge_weights (dict): co-occurence counts of the nodes in the network
+        all_tokens (list): list of all tokens in the corpus
+
+    Returns:
+        dict: key: pair of tokens, value: cosine similarity
+    """
+    token_frequency = _node_frequency_counts(all_tokens)
+    cosine_similarities = defaultdict(int)
+    for key, val in edge_weights.items():
+        cosine_similarities[key] = val / (
+            math.sqrt(token_frequency[key[0]] * token_frequency[key[1]])
+        )
+    return cosine_similarities
+
+
+def _inclusion_index(edge_weights: dict, all_tokens: list) -> dict:
+    """calculate the inclusion index between nodes in the network
+
+    Args:
+        edge_weights (dict): co-occurence counts of the nodes in the network
+        all_tokens (list): list of all tokens in the corpus
+
+    Returns:
+        dict: key: pair of tokens, value: inclusion index
+    """
+    token_frequency = _node_frequency_counts(all_tokens)
+    inclusion_index = defaultdict(int)
+    for key, val in edge_weights.items():
+        inclusion_index[key] = val / min(
+            token_frequency[key[0]], token_frequency[key[1]]
+        )
+    return inclusion_index
--- tests/networks/test_build.py
+++ tests/networks/test_build.py
@@ -2,33 +2,35 @@ import pytest
 from nesta_ds_utils.networks.build import build_coocc
 import numpy as np
 import scipy
+import networkx as nx
+import math
 
 
 def test_network_from_nested_lists():
     """tests that when a list of lists is passed to term_cooccurence_graph it returns the expected edge weights"""
     sequence = [
-        ["I", "went", "to", "the", "party"],
-        ["i", "had", "fun", "at", "the", "party"],
+        [0, 1, 2, 3, 4],
+        [0, 5, 6, 7, 3, 4],
     ]
-    network = build_coocc(sequence)
-    assert network["the"]["party"]["weight"] == 2
+    network = build_coocc(sequence, edge_attributes=["frequency"])
+    assert network[3][4]["frequency"] == 2
 
 
 def test_network_from_nested_arrays():
     """tests that when a list of arrays is passed to term_cooccurence_graph it returns the expected edge weights"""
     sequence = [
-        np.array(["I", "went", "to", "the", "party"]),
-        np.array(["i", "had", "fun", "at", "the", "party"]),
+        np.array([0, 1, 2, 3, 4]),
+        np.array([0, 5, 6, 7, 3, 4]),
     ]
-    network = build_coocc(sequence)
-    assert network["the"]["party"]["weight"] == 2
+    network = build_coocc(sequence, edge_attributes=["frequency"])
+    assert network[3][4]["frequency"] == 2
 
 
 def test_network_as_adjacency_matrix():
     """tests that when as_adj = True the function returns an adjacency matrix"""
     sequence = [
-        ["I", "went", "to", "the", "party"],
-        ["i", "had", "fun", "at", "the", "party"],
+        [0, 1, 2, 3, 4],
+        [0, 5, 6, 7, 3, 4],
     ]
     network, node_list = build_coocc(sequence, as_adj=True)
     assert isinstance(network, scipy.sparse._csr.csr_matrix)
@@ -37,18 +39,20 @@ def test_network_as_adjacency_matrix():
 def test_network_node_weight():
     """tests that when use_node_weights = True the network has the correct node weights"""
     sequence = [
-        ["I", "went", "to", "the", "party"],
-        ["i", "had", "fun", "at", "the", "party"],
+        [0, 1, 2, 3, 4],
+        [0, 5, 6, 7, 3, 4],
     ]
-    network = build_coocc(sequence, use_node_weights=True)
-    assert network.nodes["the"]["frequency"] == 2
+    network = build_coocc(
+        sequence, use_node_weights=True, edge_attributes=["frequency"]
+    )
+    assert network.nodes[3]["frequency"] == 2
 
 
 def test_directed_network():
     """tests that when directed = True the number of edges is double when the network is not directed"""
     sequence = [
-        ["I", "went", "to", "the", "party"],
-        ["i", "had", "fun", "at", "the", "party"],
+        [0, 1, 2, 3, 4],
+        [0, 5, 6, 7, 3, 4],
     ]
     directed_network = build_coocc(sequence, directed=True)
     undirected_network = build_coocc(sequence)
@@ -56,3 +60,103 @@ def test_directed_network():
     assert (
         directed_network.number_of_edges() == 2 * undirected_network.number_of_edges()
     )
+
+
+def test_jaccard_exists():
+    """tests that when jaccard similarity is used as an edge attribute it exists"""
+    sequence = [
+        [0, 1, 2, 3, 4],
+        [0, 5, 6, 7, 3, 4],
+    ]
+    network = build_coocc(sequence, edge_attributes=["jaccard"])
+    attr_list = list(list(network.edges(data=True))[0][-1].keys())
+    assert "jaccard_similarity" in attr_list
+
+
+def test_jaccard_similarity_val():
+    """tests that when the jaccard similarity is used as an edge attribute it returns the correct value"""
+    sequence = [
+        [0, 1, 2, 3, 4],
+        [0, 5, 6, 7, 3, 4],
+    ]
+    network = build_coocc(sequence, edge_attributes=["jaccard"])
+    attrs = nx.get_edge_attributes(network, "jaccard_similarity")
+    if (1, 4) in attrs.keys():
+        assert attrs[(1, 4)] == 1 / 2
+    else:
+        assert attrs[(4, 1)] == 1 / 2
+
+
+def test_association_strength_exists():
+    """tests that when association strength is used as an edge attribute it exists"""
+    sequence = [
+        [0, 1, 2, 3, 4],
+        [0, 5, 6, 7, 3, 4],
+    ]
+    network = build_coocc(sequence, edge_attributes=["association"])
+    attr_list = list(list(network.edges(data=True))[0][-1].keys())
+    assert "association_strength" in attr_list
+
+
+def test_association_strength_val():
+    """tests that when the association strength is used as an edge attribute it returns the correct value"""
+    sequence = [
+        [0, 1, 2, 3, 4],
+        [0, 5, 6, 7, 3, 4],
+    ]
+    network = build_coocc(sequence, edge_attributes=["association"])
+    attrs = nx.get_edge_attributes(network, "association_strength")
+    if (1, 4) in attrs.keys():
+        assert attrs[(1, 4)] == 1 / 2
+    else:
+        assert attrs[(4, 1)] == 1 / 2
+
+
+def test_cosine_exists():
+    """tests that when cosine similarity is used as an edge attribute it exists"""
+    sequence = [
+        [0, 1, 2, 3, 4],
+        [0, 5, 6, 7, 3, 4],
+    ]
+    network = build_coocc(sequence, edge_attributes=["cosine"])
+    attr_list = list(list(network.edges(data=True))[0][-1].keys())
+    assert "cosine_similarity" in attr_list
+
+
+def test_cosine_val():
+    """tests that when the cosine similarity is used as an edge attribute it returns the correct value"""
+    sequence = [
+        [0, 1, 2, 3, 4],
+        [0, 5, 6, 7, 3, 4],
+    ]
+    network = build_coocc(sequence, edge_attributes=["cosine"])
+    attrs = nx.get_edge_attributes(network, "cosine_similarity")
+    if (1, 4) in attrs.keys():
+        assert attrs[(1, 4)] == 1 / math.sqrt(2)
+    else:
+        assert attrs[(4, 1)] == 1 / math.sqrt(2)
+
+
+def test_inclusion_exists():
+    """tests that when the inclusion index is used as an edge attribute it exists"""
+    sequence = [
+        [0, 1, 2, 3, 4],
+        [0, 5, 6, 7, 3, 4],
+    ]
+    network = build_coocc(sequence, edge_attributes=["inclusion"])
+    attr_list = list(list(network.edges(data=True))[0][-1].keys())
+    assert "inclusion_index" in attr_list
+
+
+def test_inclusion_val():
+    """tests that when the inclusion index is used as an edge attribute it returns the correct value"""
+    sequence = [
+        [0, 1, 2, 3, 4],
+        [0, 5, 6, 7, 3, 4],
+    ]
+    network = build_coocc(sequence, edge_attributes=["inclusion"])
+    attrs = nx.get_edge_attributes(network, "inclusion_index")
+    if (1, 4) in attrs.keys():
+        assert attrs[(1, 4)] == 1
+    else:
+        assert attrs[(4, 1)] == 1
